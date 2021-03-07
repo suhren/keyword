3a51c4ca3c3a338dcd3747e54ba57758d645e8c9
suhren <adam.suhren@gmail.com>
2021-03-07 14:57:03 +0100

    Added about section in front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,11 @@ import FileCopyIcon from '@material-ui/icons/FileCopy';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 import { Button, Typography, withStyles } from '@material-ui/core';
 
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const API_BASE = 'https://keyword.bitgnd.com/'
 const API_ENDPOINT = API_BASE + 'generate'
@@ -59,8 +64,57 @@ function App() {
 
             <header className="App-header">
                 <h1 style={{margin: '1em 0 1em 0', fontSize: '1.2em'}}>
+
                     Extract <code style={{color: "#718dbd"}}>keywords</code> from text documents!
                 </h1>
+
+                <Accordion style={{backgroundColor: 'transparent'}}>
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    >
+                        <Typography style={{marginLeft: 'auto',
+                                            marginRight: 'auto',
+                                            color: '#AAA'}}>
+                            ABOUT
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography style={{fontSize: '0.6em',
+                                        color: '#AAA',
+                                        textAlign:'left'}}>
+                        This app can extract common keywords or phrases from texts. 
+                        You can do this by copying some source of text, like e.g.
+                        an article on Wikipedia, and pasting it into thetext area.
+                        Pressing the submit button will send the text to the API
+                        backend where the keywords are extracted. These aFor examplere then
+                        sent back to this interface and presented below.
+                        <br/><br/>
+                        You can view the results as 'chips' where you can also 
+                        remove results that are not of good quality by pressing
+                        the 'X' in each chip. After you are happy with the words,
+                        you can also view them as either a list or as
+                        comma-separated values (CSV) by navigating in the tab panel.
+                        <br/><br/>
+                        You can also control how the algorithm searches for keywords
+                        by changing the options below:
+                        <br/><br/>
+                        The options 1-gram, through 4-gram refers to the general
+                        concept of <a href="https://en.wikipedia.org/wiki/N-gram">n-grams</a> 
+                        in linguistics. This is just a fancy term for
+                        'a sequence of n words'. For example, a 1-gram (unigram) 
+                        would be a single word like 'Cat' or 'Run'. A 2-gram 
+                        (bigram) is sequence of two words, like 'New York' or 
+                        'Apple pie'. This same logic applies for 3- and 4-grams. By
+                        looking at the most statistically frequent n-grams, these
+                        are in practice used to find 'collocations', or common 
+                        sequences of words in the text. 
+                        <br/><br/>
+                        The results can also be be controlled by specifcying the
+                        minimum and maximum allowed number of characters per result
+                        (spaces not included).    
+                    </Typography>
+                    </AccordionDetails>
+                </Accordion>
 
                 <EditButton startIcon={<ClearIcon />}
                             onClick={() =>  {
@@ -99,7 +153,7 @@ function App() {
                                              onChange={() => {}}
                                              placeholder="Results appear here" />
                     },
-                    {
+                    { 
                         label: 'CSV',
                         component: <textarea className="textarea"
                                              rows='10'
